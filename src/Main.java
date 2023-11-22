d6fe6e05f96c93e1c714fae61a160466befeff6b
nutsour <nutsour@proton.me>
2023-11-22 17:28:03 +0800

    tmp commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("请输入选项：");
-        System.out.println("1.查看资产");
-        System.out.println("2.添加资产");
-        System.out.println("3.领用资产");
+
+        Scanner scanner = new Scanner(System.in);
 
 
         boolean flag = true;
+        while (flag){
+            System.out.println("1.查看资产");
+            System.out.println("2.添加资产");
+            System.out.println("3.领用资产");
+            System.out.println("4.退出系统");
+            System.out.println("请输入你的选择");
+            int selected = scanner.nextInt();
+            switch ( )
+        }
+
     }
 }
