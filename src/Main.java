e1a0cebaa1e243c373ecc2badbf8139193dfa342
nutsour <nutsour@proton.me>
2023-11-22 15:50:20 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("请输入选项：");
+        System.out.println("1.查看资产");
+        System.out.println("2.添加资产");
+        System.out.println("3.领用资产");
+
+
+        boolean flag = true;
+    }
+}
